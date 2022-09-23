855533b6acef82ef129763165c753ab820eebe9f
Rodolfo Carneiro de Freitas Lubeck <rcflubeck@gmail.com>
2022-09-22 22:02:28 -0300

    ajuste ignore

--- src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -11,7 +11,7 @@ public class ParkingControlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingControlApplication.class, args);
-		
+
 	}
 
 	@GetMapping("/")
